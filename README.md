a25c324acfadfca0ed0b9369add75e3235a55faf
cblkwell <cblkwell@truss.works>
2019-08-09 16:53:16 -0400

    KMS key addition (#7)
    support encrypting cloudtrail logs

--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 
 This module creates AWS CloudTrail and configures it so that logs go to cloudwatch.
 
+## Terraform Versions
+
+Terraform 0.12. Pin module version to `~> 2.0`. Submit pull-requests to `master` branch.
+
+Terraform 0.11. Pin module version to `~> 1.0`. Submit pull-requests to `terraform011` branch.
+
 ## Usage
 
 ```hcl
@@ -19,6 +25,8 @@ module "aws_cloudtrail" {
 | Name | Description | Type | Default | Required |
 |------|-------------|:----:|:-----:|:-----:|
 | cloudwatch\_log\_group\_name | The name of the CloudWatch Log Group that receives CloudTrail events. | string | `"cloudtrail-events"` | no |
+| encrypt\_cloudtrail | Whether or not to use a custom KMS key to encrypt CloudTrail logs. | string | `"false"` | no |
+| key\_deletion\_window\_in\_days | Duration in days after which the key is deleted after destruction of the resource, must be 7-30 days.  Default 30 days. | string | `"30"` | no |
 | log\_retention\_days | Number of days to keep AWS logs around in specific log group. | string | `"90"` | no |
 | org\_trail | Whether or not this is an organization trail. Only valid in master account. | string | `"false"` | no |
 | s3\_bucket\_name | The name of the AWS S3 bucket. | string | n/a | yes |
--- main.tf
+++ main.tf
@@ -4,6 +4,12 @@
  *
  * This module creates AWS CloudTrail and configures it so that logs go to cloudwatch.
  *
+ * ## Terraform Versions
+ *
+ * Terraform 0.12. Pin module version to `~> 2.0`. Submit pull-requests to `master` branch.
+ *
+ * Terraform 0.11. Pin module version to `~> 1.0`. Submit pull-requests to `terraform011` branch.
+ *
  * ## Usage
  *
  * ```hcl
@@ -27,6 +33,7 @@ data "aws_caller_identity" "current" {}
 # This section is used for allowing CloudTrail to send logs to CloudWatch.
 #
 
+# This policy allows the CloudTrail service for any account to assume this role.
 data "aws_iam_policy_document" "cloudtrail_assume_role" {
   statement {
     effect  = "Allow"
@@ -49,6 +56,10 @@ resource "aws_iam_role" "cloudtrail_cloudwatch_role" {
 resource "aws_cloudwatch_log_group" "cloudtrail" {
   name              = "${var.cloudwatch_log_group_name}"
   retention_in_days = "${var.log_retention_days}"
+
+  tags = {
+    Automation = "Terraform"
+  }
 }
 
 data "aws_iam_policy_document" "cloudtrail_cloudwatch_logs" {
@@ -78,14 +89,173 @@ resource "aws_iam_policy_attachment" "main" {
 }
 
 #
+# KMS
+#
+
+# This policy is a translation of the default created by AWS when you
+# manually enable CloudTrail; you can see it here:
+# https://docs.aws.amazon.com/awscloudtrail/latest/userguide/default-cmk-policy.html
+data "aws_iam_policy_document" "cloudtrail_kms_policy_doc" {
+  statement {
+    sid     = "Enable IAM User Permissions"
+    effect  = "Allow"
+    actions = ["kms:*"]
+
+    principals {
+      type = "AWS"
+
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid     = "Allow CloudTrail to encrypt logs"
+    effect  = "Allow"
+    actions = ["kms:GenerateDataKey*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+      values   = ["arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+    }
+  }
+
+  statement {
+    sid     = "Allow CloudTrail to describe key"
+    effect  = "Allow"
+    actions = ["kms:DescribeKey"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Allow principals in the account to decrypt log files"
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt",
+      "kms:ReEncryptFrom",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = ["*"]
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = ["${data.aws_caller_identity.current.account_id}"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+      values   = ["arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+    }
+  }
+
+  statement {
+    sid     = "Allow alias creation during setup"
+    effect  = "Allow"
+    actions = ["kms:CreateAlias"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:ViaService"
+      values   = ["ec2.${data.aws_region.current.name}.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = ["${data.aws_caller_identity.current.account_id}"]
+    }
+
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "Enable cross account log decryption"
+    effect = "Allow"
+
+    actions = [
+      "kms:Decrypt",
+      "kms:ReEncryptFrom",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = ["${data.aws_caller_identity.current.account_id}"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+      values   = ["arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"]
+    }
+
+    resources = ["*"]
+  }
+}
+
+resource "aws_kms_key" "cloudtrail" {
+  count                   = "${var.encrypt_cloudtrail ? 1 : 0}"
+  description             = "A KMS key used to encrypt CloudTrail log files stored in S3."
+  deletion_window_in_days = "${var.key_deletion_window_in_days}"
+  enable_key_rotation     = "true"
+  policy                  = "${data.aws_iam_policy_document.cloudtrail_kms_policy_doc.json}"
+
+  tags = {
+    Automation = "Terraform"
+  }
+}
+
+resource "aws_kms_alias" "cloudtrail" {
+  count         = "${var.encrypt_cloudtrail ? 1 : 0}"
+  name          = "alias/cloudtrail"
+  target_key_id = "${aws_kms_key.cloudtrail[0].key_id}"
+}
+
+#
 # CloudTrail
 #
 
 resource "aws_cloudtrail" "main" {
+  name = "cloudtrail"
+
+  # Send logs to CloudWatch Logs
   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail.arn}"
   cloud_watch_logs_role_arn  = "${aws_iam_role.cloudtrail_cloudwatch_role.arn}"
 
-  name           = "cloudtrail"
+  # Send logs to S3
   s3_key_prefix  = "cloudtrail"
   s3_bucket_name = "${var.s3_bucket_name}"
 
@@ -99,7 +269,14 @@ resource "aws_cloudtrail" "main" {
   # enable log file validation to detect tampering
   enable_log_file_validation = true
 
+  kms_key_id = "${var.encrypt_cloudtrail ? aws_kms_key.cloudtrail[0].arn : null}"
+
   tags = {
     Automation = "Terraform"
   }
+
+  depends_on = [
+    "aws_kms_key.cloudtrail",
+    "aws_kms_alias.cloudtrail",
+  ]
 }
