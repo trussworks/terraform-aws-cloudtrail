f5789293afac21324aa7290a9fa25364af891032
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2019-11-04 13:42:20 -0800

    Add terratest plumbing
    Prior to this change, the module did not have any terratesting
    infrastructure in place.
    
    This change adds a simple terratest example, as well as config files for
    dependabot and golangci.
    
    https://www.pivotaltracker.com/story/show/169409905

---
+++ examples/simple/main.tf
@@ -0,0 +1,19 @@
+# Referencing module.logs.aws_logs_bucket directly in the cloudtrail module
+# will fail with an InsufficientS3BucketPolicyException. Using the data
+# source as a workaround to ensure the S3 bucket policy has propagated before
+# the cloudtrail module references it.
+module "aws_cloudtrail" {
+  source         = "../../"
+  s3_bucket_name = data.aws_s3_bucket.new_logs_bucket.id
+}
+
+module "logs" {
+  source         = "trussworks/logs/aws"
+  version        = "4.0.0"
+  s3_bucket_name = var.logs_bucket
+  region         = var.region
+}
+
+data "aws_s3_bucket" "new_logs_bucket" {
+  bucket = module.logs.aws_logs_bucket
+}
---
+++ test/terraform_aws_cloudtrail_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestTerraformAwsCloudtrail(t *testing.T) {
+	t.Parallel()
+
+	bucket := fmt.Sprintf("terratest-aws-cloudtrail-%s", strings.ToLower(random.UniqueId()))
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: "../examples/simple/",
+		Vars: map[string]interface{}{
+			"logs_bucket": bucket,
+			"region":      awsRegion,
+		},
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Empty logs_bucket before terraform destroy
+	aws.EmptyS3Bucket(t, awsRegion, bucket)
+}
